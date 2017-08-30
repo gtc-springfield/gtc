3964495017a9e9ba55dabb84928043c4f7cea0e7
rautey <IRaut16@MassMutual.com>
2017-08-30 14:14:20 -0400

    added data loading script

---
+++ data_loading.R
@@ -0,0 +1,37 @@
+
+#-----------------------------------------------------------------------------#
+# UTILITY FUNCTIONS FOR DATA LOADING
+#-----------------------------------------------------------------------------#
+library(dplyr)
+library(readr)
+library(tidyr)
+
+
+
+# make a function to read and clean the data 
+read_clean_data<- function(infile) {
+
+  #add code to make sure the files in each folder are as one dataframe
+}
+  
+  
+  # Read in the item data
+  
+  datadir <- "~/Desktop/gtc/square/data/items"
+  files <- list.files(datadir, pattern = ".csv", full.names = TRUE)
+  
+  for (file in files) {
+    data <- read_csv(file)
+    
+  }
+  
+  # Read in the transactions data
+  
+  datadir <- "~/Desktop/gtc/square/data/transactions"
+  files <- list.files(datadir, pattern = ".csv")
+  
+  for (file in files) {
+    data <- read_csv(file)
+    
+  }
+  
