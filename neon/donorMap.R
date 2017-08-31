391202ee3e9a5b4fb4cbe08d845ee79c5087df65
mlaethem <MLaethem76@MassMutual.com>
2017-08-31 14:27:26 -0400

    Update donorMap.R

--- neon/donorMap.R
+++ neon/donorMap.R
@@ -2,27 +2,27 @@ library(leaflet)
 
 #Geocoding the addresses
 #Combine addresses into one column
-df$address <- paste(df$`Address Line 1`, df$City, df$State, sep = ', ')
+donations$address <- paste(donations$Address.Line.1, donations$City, donations$State, sep = ', ')
 #Remove NAs
-df$address <- gsub('NA,|,NA|NA', '', df$address)
+donations$address <- gsub('NA,|,NA|NA', '', donations$address)
 
 #Add new dataframe with only unique addresses
-geoCode <- distinct(df, address)
+geoCode <- distinct(donations, address)
 
 # Loop through the addresses to get the latitude and longitude of each address
 # and add it to the df data frame in new columns lat and lon
 for(i in 1:nrow(geoCode))
 {
   result <- geocode(geoCode$address[i], output = "latlona", source = "google")
-  df$lon[i] <- as.numeric(result[1])
-  df$lat[i] <- as.numeric(result[2])
+  geoCode$lon[i] <- as.numeric(result[1])
+  geoCode$lat[i] <- as.numeric(result[2])
 }
 
 #Leaflet map of donors
 map <- leaflet() %>%
        addTiles() %>%  # Add default OpenStreetMap map tiles
-       addCircleMarkers(lng = df$lon,
-                        lat = df$lat,
+       addCircleMarkers(lng = geoCode$lon,
+                        lat = geoCode$lat,
                         color = "purple",
                         radius = 5,
                         stroke = FALSE,
