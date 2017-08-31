960a30837fb90a103d176fc83c86747e451d18fa
Matt Girard <MGirard59@MassMutual.com>
2017-08-30 21:09:36 -0400

    Refined data_loading

--- square/vis/data_loading.R
+++ square/vis/data_loading.R
@@ -8,21 +8,21 @@ library(tidyr)
 
 
 
-# make a function to read and clean the data 
-read_clean_data<- function() {
-  datadir_items <- "../data/items"
-  datadir_trans <- "../data/trans"
-  
+# make a function to read and clean the data
+read_clean_data <- function() {
+  datadir_items <- "data/items"
+  datadir_trans <- "data/transactions"
+
   raw_items <- get_raw_data(datadir_items)
   raw_trans <- get_raw_data(datadir_trans)
   return(list(raw_items, raw_trans))
 
   #add code to make sure the files in each folder are as one dataframe
 }
-  
-  
+
+
   # Read in data either in items or trans
-  
+
 get_raw_data <- function(datadir){
   files <- list.files(datadir, pattern = ".csv", full.names = TRUE)
   n_files = 0
@@ -33,17 +33,16 @@ get_raw_data <- function(datadir){
     }
     else{
       data_more <- read.csv(file)
-      data <- full_join(data_more, data, by = "Date")
+      data <- rbind(data, data_more)
       rm(data_more)
       n_files = n_files + 1
     }
-    
+
   }
   return(data)
-  
+
 }
-  
-  
-  
-  
-  
+
+
+
+
